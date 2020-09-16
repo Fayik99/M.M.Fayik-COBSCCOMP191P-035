5cb9b442ee18acb20a0514d62b6737ab3b8188b0
Fayik99 <32995747+Fayik99@users.noreply.github.com>
2020-09-16 16:11:40 +0530

    Notifications view UI done

--- M.M.Fayik-COBSCCOMP191P-035/Controllers/CreateNotificationsViewController.swift
+++ M.M.Fayik-COBSCCOMP191P-035/Controllers/CreateNotificationsViewController.swift
@@ -27,6 +27,44 @@ class CreateNotificationsViewController: UIViewController {
         return button
     }()
     
+    private let notificationUpdateUIView: UIView = {
+        
+        let tileView = UIView()
+        tileView.backgroundColor = .white
+        tileView.layer.cornerRadius = 5
+        tileView.layer.masksToBounds = true
+        return tileView
+    }()
+    
+    private let createNotificationTF: UITextField = {
+        
+        let name = UITextField()
+        name.borderStyle = .roundedRect
+        name.font = UIFont(name: "Avenir-medium", size: 18)
+        name.textColor = .black
+        name.keyboardAppearance = .dark
+        name.isSecureTextEntry = false
+        name.placeholder = "Type here..."
+        name.textAlignment = .left
+        
+        return name
+    }()
+    
+    private let SubmitNotificationsBtn: AuthUIButton = {
+        
+        let button = AuthUIButton(type: .system)
+        button.setTitle("C R E A T E", for: .normal)
+        button.backgroundColor = UIColor.white
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth  = 1.0
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitleColor(UIColor(white: 0.5, alpha: 1.5), for: .normal)
+       // button.addTarget(self, action: #selector(TempUpdateFb), for: UIControl.Event.touchUpInside)
+        
+        return button
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -40,9 +78,19 @@ class CreateNotificationsViewController: UIViewController {
         BackButton.anchor(top: view.safeAreaLayoutGuide.topAnchor,left: view.leftAnchor, paddingTop: 5, paddingLeft: 12, width: 30, height: 25)
         
         view.addSubview(titleLabel)
-        titleLabel.anchor(top: BackButton.bottomAnchor, left: view.leftAnchor, paddingTop: 15, paddingLeft: 16)
+        titleLabel.anchor(top: BackButton.bottomAnchor, left: view.leftAnchor, paddingTop: 18, paddingLeft: 16)
         titleLabel.centerX(inView: view)
-    
+        
+        view.addSubview(notificationUpdateUIView)
+        notificationUpdateUIView.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 22, paddingLeft: 12, paddingRight: 12, height: 250)
+        
+        view.addSubview(createNotificationTF)
+        createNotificationTF.anchor(top: notificationUpdateUIView.topAnchor, left: notificationUpdateUIView.leftAnchor, right: notificationUpdateUIView.rightAnchor, paddingTop: 12, paddingLeft: 14, paddingRight: 14, height: 120)
+        
+        
+        view.addSubview(SubmitNotificationsBtn)
+        SubmitNotificationsBtn.anchor(top: createNotificationTF.bottomAnchor, left: notificationUpdateUIView.leftAnchor, right: notificationUpdateUIView.rightAnchor, paddingTop: 25, paddingLeft: 70, paddingRight: 70, height: 50)
+
     }
     
     @objc func showUpdateController() {
